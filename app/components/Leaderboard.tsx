39076263defc3c609b46cf811820b81f04457992
Seif Shahwan <saif.shahwan@icloud.com>
2024-05-22 12:39:16 +0200

    chore: Update Home page layout and Leaderboard component styles

--- app/components/Leaderboard.tsx
+++ app/components/Leaderboard.tsx
@@ -21,7 +21,7 @@ const Leaderboard: React.FC = async () => {
   }));
 
   return (
-    <div className="max-w-4xl mx-auto p-4 text-black">
+    <div className="max-w-full mx-auto p-4 text-black">
       <h1 className="text-xl font-bold text-center mb-4">Leaderboard</h1>
       <div className="overflow-x-auto">
         <table className="table-auto w-full">
