65521b7d7492a9b41bf91c59185b3efb30c83b52
Seif Shahwan <Shahwan@helden-im-handwerk.de>
2024-06-28 04:07:56 +0200

    tested fetching the images on the registration component instead of the game page

--- app/actions.ts
+++ app/actions.ts
@@ -5,7 +5,7 @@ import { createSSRClient } from "@/app/utils/supabaseServer";
 
 
 // user registration
-export default async function registerUser(username: string) {
+export  async function registerUser(username: string) {
   const client = createSSRClient();
   try {
       const { data, error } = await client.from('users').insert([{ username }]).select();
@@ -26,6 +26,23 @@ export default async function registerUser(username: string) {
 
 
 //image fetching
+export async function fetchImages() {
+  const client = createSSRClient();
+  try {
+      const { data, error } = await client.from("images").select("*").order("id", { ascending: true });
+      if (error) {
+          console.error('Error fetching images:', error);
+          return { error: 'Error fetching images', details: error.message };
+      }
+      if (!data) {
+          return { error: 'No image found' };
+      }
+      return { data };
+  } catch (exception) {
+      console.error('Exception in GET API:', exception);
+      return { error: 'Server error', details: (exception as Error).message };
+  }
+}
 
 
 
